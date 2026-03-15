676360089a98b4d8223d45b9251c2a20709f2d1d
clshu <clshu2020@gmail.com>
2026-03-15 04:05:39 -0400

    v-on and @ on property value, either numeric and boolean value v-if

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,22 @@ const message = ref('Hello World')
 const state = reactive({
   count: 0,
 })
+const isButtonDisabled = ref(false)
 </script>
 
 <template>
   <h1>Learn Vue</h1>
   <p>{{ message }}</p>
-  <p>{{ state.count }}</p>
-  <button @click="state.count++">Increment</button>
-  <button @click="state.count--">Decrement</button>
+  <p>Count: {{ state.count }}</p>
+  <button @click="state.count++" :disabled="isButtonDisabled">Increment</button>
+  <button @click="state.count--" :disabled="isButtonDisabled">Decrement</button>
+
+  <br />
+  <button @click="isButtonDisabled = !isButtonDisabled">
+    {{ isButtonDisabled ? 'Enable' : 'Disable' }} Counter Buttons
+  </button>
+  <hr />
+  <p v-if="state.count === 3">Count is 3</p>
 </template>
 
 <style scoped></style>
