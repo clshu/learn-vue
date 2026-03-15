76e7ab48a077a0e64477477127b5e6bcb2f29117
clshu <clshu2020@gmail.com>
2026-03-15 02:45:42 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const message = ref('Hello World')
+const state = reactive({
+  count: 0,
+})
+</script>
+
+<template>
+  <h1>Learn Vue</h1>
+  <p>{{ message }}</p>
+  <p>{{ state.count }}</p>
+  <button @click="state.count++">Increment</button>
+  <button @click="state.count--">Decrement</button>
+</template>
+
+<style scoped></style>
