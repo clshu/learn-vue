9ba2122169a924348ad47b56da0a3472048458b8
clshu <clshu2020@gmail.com>
2026-03-16 10:55:38 -0400

    Markdown Editor

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import AttributeBindings from './components/basic/AttributeBindings.vue'
 import ConditionalsAndLoops from './components/basic/ConditionalsAndLoops.vue'
 import FormBindings from './components/basic/FormBindings.vue'
 import SimpleComponent from './components/basic/SimpleComponent.vue'
+import MarkdownEditor from './components/practical/MarkdownEditor.vue'
 
 const componentName = ref('')
 const currentComponent = ref<Component | null>(null)
@@ -40,10 +41,19 @@ const basicComponents = [
   },
 ]
 
+const practicalComponents = [
+  {
+    name: 'MarkdownEditor',
+    component: MarkdownEditor,
+  },
+]
+
 const getComponents = (category: string) => {
   switch (category) {
     case 'basic':
       return basicComponents
+    case 'practical':
+      return practicalComponents
     default:
       return []
   }
@@ -64,6 +74,10 @@ const findComponent = (category: string, name: string) => {
     <h3>Basic</h3>
     <ul v-for="component in basicComponents" :key="component.name">
       <li @click="showComponent('basic', component.name)">{{ component.name }}</li>
+    </ul>
+    <h3>Practical</h3>
+    <ul v-for="component in practicalComponents" :key="component.name">
+      <li @click="showComponent('practical', component.name)">{{ component.name }}</li>
     </ul>
   </div>
   <div v-else>
